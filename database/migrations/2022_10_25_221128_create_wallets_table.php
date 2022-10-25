414fa4b54ad6eafe4f51fcc8c749c6da8a3f3c0b
saadKhan7890 <saadKhan7890@gmail.com>
2022-10-26 03:08:14 +0500

    other database update

---
+++ database/migrations/2022_10_25_221128_create_wallets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWalletsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wallets', function (Blueprint $table) {
+            $table->id();
+            $table->string('wallet_name', 225);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wallets');
+    }
+}
